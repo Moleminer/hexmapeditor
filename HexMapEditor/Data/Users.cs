e8a12f1374a307224a622dc9563c9000b22b66fa
Owen <s4008352@student.rmit.edu.au>
2026-03-29 13:01:23 +1100

    Added user json file, and reading in of it

---
+++ HexMapEditor/Data/Users.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace HexMapEditor.Data;
+
+
+public static class Users
+{
+	
+	public static JsonObject getUsers()
+	{
+		StreamReader sr = new StreamReader(@"Content\users.json");
+		
+		JsonObject j = JsonNode.Parse(sr.ReadToEnd()).AsObject();
+		// Console.WriteLine(j[0].AsObject());
+		return j[0].AsObject();
+	}
+
+	
+
+	public static string ToJson()
+	{
+		return System.Text.Json.JsonSerializer.Serialize("grid");
+	}
+
+}
